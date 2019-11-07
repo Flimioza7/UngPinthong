b20a35c415be4334da5e7f2ee6357006414d321c
Notebook IT Spare <nb-it@pipestate.com>
2019-11-07 15:02:48 +0700

    Post Data To server

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:ungpinthong/utility/my_style.dart';
 import 'package:intl/intl.dart';
 
@@ -8,6 +11,8 @@ class Register extends StatefulWidget {
 }
 
 //field
+final formkey = GlobalKey<FormState>();
+String name, user, password, datestring;
 
 //method
 
@@ -16,19 +21,29 @@ Widget nameText() {
     margin: EdgeInsets.only(left: 30.0, right: 30.0),
     child: TextFormField(
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.face,
-            size: 36.0,
-            color: Colors.red,
-          ),
-          labelText: 'Display Name',
-          labelStyle: TextStyle(
-            color: Colors.red,
-          ),
-          helperText: 'imput data',
-          helperStyle: TextStyle(
-            color: Colors.red,
-          )),
+        icon: Icon(
+          Icons.face,
+          size: 36.0,
+          color: Colors.red,
+        ),
+        labelText: 'Display Name',
+        labelStyle: TextStyle(
+          color: Colors.red,
+        ),
+        helperText: 'imput data',
+        helperStyle: TextStyle(
+          color: Colors.red,
+        ),
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please Fill Name in the Bank';
+        } else {
+          return null;
+        }
+      },onSaved: (value){
+        name=value.trim();
+      },
     ),
   );
 }
@@ -37,20 +52,34 @@ Widget userText() {
   return Container(
     margin: EdgeInsets.only(left: 30.0, right: 30.0),
     child: TextFormField(
+      keyboardType: TextInputType.emailAddress, //เพิ่ม @
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.account_box,
-            size: 36.0,
-            color: Colors.blue,
-          ),
-          labelText: 'User Name',
-          labelStyle: TextStyle(
-            color: Colors.blue,
-          ),
-          helperText: 'imput Name',
-          helperStyle: TextStyle(
-            color: Colors.blue,
-          )),
+        icon: Icon(
+          Icons.account_box,
+          size: 36.0,
+          color: Colors.blue,
+        ),
+        labelText: 'User Name',
+        labelStyle: TextStyle(
+          color: Colors.blue,
+        ),
+        helperText: 'Email ถูกต้อง',
+        helperStyle: TextStyle(
+          color: Colors.blue,
+        ),
+        hintText: 'ss@email.com',
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'กรุณากรอกEmail';
+        } else if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'กรุณาตรวจสอบอีเมล์';
+        } else {
+          return null;
+        }
+      },onSaved: (value){
+        user=value.trim();
+      },
     ),
   );
 }
@@ -73,7 +102,16 @@ Widget passwordText() {
           helperStyle: TextStyle(
             color: Colors.green,
           ),
-          hintText: 'more 6 charatir'),
+          hintText: 'more 6 charati'),
+      validator: (value) {
+        if (value.length < 6) {
+          return 'pless input ';
+        } else {
+          return null;
+        }
+      },onSaved: (value){
+        password=value.trim();
+      },
     ),
   );
 }
@@ -82,7 +120,7 @@ Widget showcerrendate() {
   DateTime dateTime = DateTime.now();
   print('DateTime = $dateTime');
 
-  String datestring = DateFormat('yyy-MM-dd').format(dateTime);
+  datestring = DateFormat('yyy-MM-dd').format(dateTime);
   print('DateString = $datestring');
 
   return Text(
@@ -91,13 +129,37 @@ Widget showcerrendate() {
   );
 }
 
-Widget regsiterBudton() {
+Widget regsiterBudton(BuildContext context) {
   return IconButton(
     tooltip: 'Uplode to server',
     icon: Icon(Icons.cloud_upload),
-    onPressed: () {},
+    onPressed: () {
+      print('You click register');
+      if (formkey.currentState.validate()) {
+        formkey.currentState.save();
+        print('name=$name, user=$user,pass=$password');
+        registerthread(context);
+      }
+    },
   );
 }
+
+Future<void>registerthread(BuildContext context)async{
+String url='https://www.androidthai.in.th/pint/adduserfilm.php?isAdd=true&name=$name&user=$user&password=$password&regisdate=$datestring';
+Response response = await get(url);
+print('response=$response');
+//var รับค่าอะไรก็ได้
+var result = json.encode(response.body);
+print('resule=$result');
+
+  if (result.toString()=='true') {
+    Navigator.of(context).pop();
+  } else {
+    
+  }
+
+}
+
 
 class _RegisterState extends State<Register> {
   @override
@@ -106,27 +168,30 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         backgroundColor: MyStyle().textColor,
         title: Text('Register'),
-        actions: <Widget>[regsiterBudton()],
+        actions: <Widget>[regsiterBudton(context)],
       ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.white,Colors.white, MyStyle().mainColor],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter //ไล่สีบนลงล่าง
-          ),
+              colors: [Colors.white, Colors.white, MyStyle().mainColor],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter //ไล่สีบนลงล่าง
+              ),
         ),
-        child: ListView(
-          padding: EdgeInsets.all(30.0),
-          children: <Widget>[
-            showcerrendate(),
-            SizedBox(
-              height: 20.0,
-            ),
-            nameText(),
-            userText(),
-            passwordText(),
-          ],
+        child: Form(
+          key: formkey,
+          child: ListView(
+            padding: EdgeInsets.all(30.0),
+            children: <Widget>[
+              showcerrendate(),
+              SizedBox(
+                height: 20.0,
+              ),
+              nameText(),
+              userText(),
+              passwordText(),
+            ],
+          ),
         ),
       ),
     );
