cf011cf9d3ab0fcad40260c38a327da007ee3af2
Notebook IT Spare <nb-it@pipestate.com>
2019-11-08 15:15:50 +0700

    Disign UI ReadQR

--- lib/widget/page2.dart
+++ lib/widget/page2.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:ungpinthong/utility/my_style.dart';
 
 class Page2 extends StatefulWidget {
   @override
   _Page2State createState() => _Page2State();
 }
 
+//field
+String codestring = '';
+
+//method
+Widget readQRbudton() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: <Widget>[
+      Row(
+        children: <Widget>[
+          Expanded(
+                      child: RaisedButton.icon(
+              icon: Icon(Icons.photo_camera),
+              label: Text('Read QR Code'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget codereaded() {
+  return Center(
+    child: Text(
+      'Code Readed $codestring',
+      style: TextStyle(color: MyStyle().textColor, fontSize: MyStyle().h2),
+    ),
+  );
+}
+
+Widget headtitel() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'Read Barcode or QR Code',
+        style: TextStyle(fontSize: MyStyle().h1),
+      ),
+    ],
+  );
+}
+
 class _Page2State extends State<Page2> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Page2'),
+      child: Stack(
+        children: <Widget>[headtitel(), codereaded(), readQRbudton()],
+      ),
     );
   }
 }
